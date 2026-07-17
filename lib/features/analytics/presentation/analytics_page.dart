ec2d17790a538d0d0d66ce071a36199236ea779d
bxyz1000 <bxyz1000@gmail.com>
2026-07-17 10:01:16 +0530

    Initial commit

--- lib/features/analytics/presentation/analytics_page.dart
+++ lib/features/analytics/presentation/analytics_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../tasks/data/task_repository.dart';
 import '../../habits/data/habit_repository.dart';
